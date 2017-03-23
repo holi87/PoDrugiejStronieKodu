1ce4710ce8ed1ae4570e23ab9acd59834287c0f1
holi87 <holi87@gmail.com>
2017-03-23 07:54:55 +0100

    czwarty test - dzielenie przez zero

--- modul.Unit.py
+++ modul.Unit.py
@@ -9,8 +9,11 @@ class TestyModul(unittest.TestCase):
     def test_czy_dzielenie_12_przez_4_da_3(self):
         self.assertEqual(3, modul.dzielenie(12, 4))
 
-    def test_czy_dzielenie_12_przez_3_da_3(self):
+    def test_czy_dzielenie_12_przez_3_da_3(self):  # dzielnik zwieksza sie o 1, czyli de fato 12/4 = 3
         self.assertEqual(3, modul.dzielenie(12, 3))
+
+    def test_czy_dzielenie_20_przez_0_da_20(self):  # dzielnik = 0, wiec zwraca dzielna
+        self.assertEqual(20, modul.dzielenie(20, 0))
 
 if __name__ == '__main__':
     unittest.main()
