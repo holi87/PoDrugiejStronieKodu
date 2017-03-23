23ec8931dc456920ffc25fda1626704157bb71ca
holi87 <holi87@gmail.com>
2017-03-23 06:55:31 +0100

    pierwszy test - sprawdzenie czy dzielenie zwroci wynik zmiennoprzecinkowy

--- modul.Unit.py
+++ modul.Unit.py
@@ -2,8 +2,8 @@ import unittest
 
 
 class TestyModul(unittest.TestCase):
-    def test_przykladowy(self):
-        pass
+    def test_czy_dzielenie_4_przez_2_da_float(self):
+        self.assertTrue(isinstance(modul.dzielenie(4,2), float))
 
 if __name__ == '__main__':
     unittest.main()
