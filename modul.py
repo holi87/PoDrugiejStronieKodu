b25df5cf4cc00d27ca9d56780f823a6bad7047de
holi87 <holi87@gmail.com>
2017-03-23 07:56:07 +0100

    kod do czwartego testu

--- modul.py
+++ modul.py
@@ -4,6 +4,8 @@ __author__ = "Grzegorz Holak"
 
 def dzielenie(x, y):
     if y % 2 == 0:
+        if y == 0:
+            return x
         return x / y
     else:
         return x / zrob_parzysta(y)
