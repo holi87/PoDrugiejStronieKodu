4b7bed324ffa6b9fccbe738bae91b4f5a4da7a3f
holi87 <holi87@gmail.com>
2017-03-23 07:04:24 +0100

    i kod do 3 testu

--- modul.py
+++ modul.py
@@ -1,6 +1,9 @@
-
+from biblioteka import zrob_parzysta
 __author__ = "Grzegorz Holak"
 
 
 def dzielenie(x, y):
-    return x / y
+    if y % 2 == 0:
+        return x / y
+    else:
+        return x / zrob_parzysta(y)
